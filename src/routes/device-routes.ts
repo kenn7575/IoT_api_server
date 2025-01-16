3f48781fdd4fd2806f5042c14a4905fd2d2448b6
Kenni Kollemorten <kenn7575@gmail.com>
2025-01-16 11:08:36 +0100

    chore: Update device-routes.ts to use registerDevice controller

--- src/routes/device-routes.ts
+++ src/routes/device-routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { deviceLoginController } from "../controllers/device_login_controller";
+import { registerDevice } from "../controllers/register_devices_controller";
 
 const router = express.Router();
 
-router.post("/" /* controller */); //register new device
+router.post("/", registerDevice); //register new device
 router.post("/login", deviceLoginController); //login device and return API key
 
 export default router;
--- src/routes/register_devices_route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerDevice } from "../controllers/register_devices_controller";
-
-const router = Router();
-
-router.post("/register-device", registerDevice);
-
-export default router;
