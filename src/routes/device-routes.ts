0e5ffc74fc9ddd95de2d21e49d2af4cff0cbdb9c
Kenni Kollemorten <kenn7575@gmail.com>
2025-01-20 09:17:50 +0100

    feat(routes): add endpoints for retrieving and updating sensor settings

--- src/routes/device-routes.ts
+++ src/routes/device-routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { deviceLoginController } from "../controllers/device_login_controller";
 import { registerDevice } from "../controllers/register_devices_controller";
+import { getSettings } from "../controllers/get_settings_controller";
+import { updateSensorSettings } from "../controllers/update_settings_controller";
 
 const router = express.Router();
 
-router.post("/", registerDevice); //register new device
+router.post("/:machine_id", registerDevice); //register new device
 router.post("/login", deviceLoginController); //login device and return API key
+
+router.get("/:machine_id/settings", getSettings); //register new device
+router.patch("/:machine_id/settings", updateSensorSettings); //login device and return API key
 
 export default router;
