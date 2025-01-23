769ca3c8e9cf5ed189dacc6fb484854fe21c2ebc
Nev0ka <nicolai@osberg.info>
2025-01-23 13:54:36 +0100

    Added device info update to device routes

--- src/routes/device-routes.ts
+++ src/routes/device-routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { deviceLoginController } from "../controllers/device_login_controller";
 import { registerDevice } from "../controllers/register_devices_controller";
+import { updateDeviceInfo } from "../controllers/device/update_device_info_controller";
 import { getSettings } from "../controllers/get_settings_controller";
 import { updateSettingsController } from "../controllers/settings/update_settings_controller";
 import { authMiddleware } from "../middlewares/auth";
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.post("/", registerDevice); //register new device
 router.post("/login", deviceLoginController); //login device and return API key
+router.patch("/:machine_id", authMiddleware, updateDeviceInfo); //update device info
 
 router.get("/:machine_id/settings", authMiddleware, getSettings); //register new device
 router.patch("/:machine_id/settings", authMiddleware, updateSettingsController);
